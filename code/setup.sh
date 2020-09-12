c0d12cb61e85e2ff21f576eded5ca6ab1b206a7f
Lucca Pessoa <luccapessoamatos@gmail.com>
2020-09-12 11:21:22 -0300

    fix: remove git config setup

--- code/setup.sh
+++ code/setup.sh
@@ -321,42 +321,6 @@ function OhMyZSHThemes(){
 }
 
 function Config(){
-  Status "👾 Config Git"
-
-  cat <<- _EOF_ >$HOME/.gitconfig
-[user]
-  name = Lucca Pessoa
-  email = luccapessoamatos@gmail.com
-[core]
-  editor = vim
-[alias]
-  co = checkout
-  ci = commit
-  br = branch
-  st = status
-  unstage = reset HEAD
-  last = log -1 HEAD
-  lg = log --color --graph --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-[color]
-  status = always
-[include]
-  path = ./.gitconfig.local
-_EOF_
-
-  git config --global url.ssh://git@git.stefanini.io/.insteadOf https://git.stefanini.io/
-
-  Status "👾 Config Git Credentials"
-  git_config_local_file=$HOME/.gitconfig.local
-  if [ ! -f "$git_config_local_file" ]; then
-    read -rn 1 -p "Save git credentials to an unencrypted file to avoid writing? [y/N] " git_save_credential
-    if [[ $git_save_credential =~ ^([Yy])$ ]]; then
-      echo -e "[credential]\nhelper = store\n" > $git_config_local_file
-      echo -e "\nWill save git credentials to an unencrypted file"
-    else
-      echo -e "[credential]\nhelper = cache --timeout 7200\n" > $git_config_local_file
-      echo -e "\nWill cache credentails for two hours"
-    fi
-  fi
 
   # Use zsh as default shell
   Status "👾 Config ZSH"
